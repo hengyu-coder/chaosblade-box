4d3c20b8a0b121e0240d9aecec75605e7555837d
hengyu-coder <guoyu486@gmail.com>
2022-07-05 15:46:41 +0800

    feature: optize yh logic
    Signed-off-by: hengyu-coder <guoyu486@gmail.com>

--- chaosblade-box-starter/src/main/java/com/alibaba/chaosblade/box/Application.java
+++ chaosblade-box-starter/src/main/java/com/alibaba/chaosblade/box/Application.java
@@ -1,11 +1,13 @@
 package com.alibaba.chaosblade.box;
 
+import com.cas.starter.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = {"com.alibaba.chaosblade.box"})
 @PropertySource(value = {"application.properties"})
+@EnableCasClient
 public class Application {
 
     public static void main(String[] args) {
--- chaosblade-box-starter/src/main/java/com/alibaba/chaosblade/box/auth/CloudLoginUserResolver.java
+++ chaosblade-box-starter/src/main/java/com/alibaba/chaosblade/box/auth/CloudLoginUserResolver.java
@@ -2,33 +2,54 @@ package com.alibaba.chaosblade.box.auth;
 
 import com.alibaba.chaosblade.box.common.common.domain.user.ChaosUser;
 import com.alibaba.chaosblade.box.common.common.enums.CommonErrorCode;
+import com.alibaba.chaosblade.box.common.common.util.EncryptUtil;
 import com.alibaba.chaosblade.box.common.infrastructure.exception.ChaosException;
 import com.alibaba.chaosblade.box.common.infrastructure.exception.PermissionDeniedException;
+import com.alibaba.chaosblade.box.service.UserService;
+
 import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author haibin
- * 
- *
  */
 @Slf4j
 @Component
 public class CloudLoginUserResolver implements LoginUserResolver {
 
     private static final List<String> WHITE_LIST = ImmutableList.of();
+    @Resource
+    private UserService userService;
 
     @Override
-    public ChaosUser resolve(HttpServletRequest httpServletRequest) throws ChaosException {
+    public ChaosUser resolve(HttpServletRequest httpServletRequest)
+        throws Exception {
         String path = httpServletRequest.getRequestURI();
         if (WHITE_LIST.contains(path)) {
             return null;
+        }
+        AttributePrincipal principal = (AttributePrincipal)httpServletRequest.getUserPrincipal();
+        if (principal == null || principal.getAttributes().isEmpty()) {
+            throw new PermissionDeniedException(CommonErrorCode.P_LOGIN_MISSED, null);
         }
 
         HttpSession session = httpServletRequest.getSession();
@@ -36,8 +57,8 @@ public class CloudLoginUserResolver implements LoginUserResolver {
         String name = "";
         String license = "";
         ChaosUser chaosUser = new ChaosUser();
-        if (session != null){
-            Enumeration<String> enumerations =  session.getAttributeNames();
+        if (session != null) {
+            Enumeration<String> enumerations = session.getAttributeNames();
             while (enumerations.hasMoreElements()) {
                 String element = enumerations.nextElement();
                 if (element.equals("uid")) {
@@ -51,15 +72,11 @@ public class CloudLoginUserResolver implements LoginUserResolver {
                 }
             }
         }
-
+        ChaosUser user = null;
         if (uid.equals("")) {
-            throw new PermissionDeniedException(CommonErrorCode.P_LOGIN_MISSED, null);
+            user = userService.login("admin", EncryptUtil.reEncryptPassword("admin"));
         }
-
-        chaosUser.setUserId(uid);
-        chaosUser.setUserName(name);
-        chaosUser.setLicense(license);
-        return chaosUser;
+        return user;
     }
 
 }
